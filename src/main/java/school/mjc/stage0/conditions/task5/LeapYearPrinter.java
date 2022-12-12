293c39035649acf9422164435c377ad3d6c27fde
Staluks <janchukssss@inbox.lv>
2022-12-12 15:18:17 +0200

    completed 4.5task

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int leapYear = year%4;
+        switch (leapYear){
+            case 0-> System.out.println("leap");
+            default -> System.out.println("not leap");
+        }
     }
 }
